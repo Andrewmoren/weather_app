61fdc1fb990a463ff06fc9104b58af30d12e86e9
Andrew <andrewmoren93@gmail.com>
2023-07-26 23:12:23 +0300

    add interval

--- src/Utils/useDate.jsx
+++ src/Utils/useDate.jsx
@@ -1,6 +1,16 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const useDate = () => {
   const locale = "en";
   const [today, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDate(new Date());
+    }, 60 * 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 };
