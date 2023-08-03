0844cdf0c5a5de9c2cd07f16d0470059eb431f5e
Andrew <andrewmoren93@gmail.com>
2023-08-03 19:13:43 +0300

    add state

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,4 +4,9 @@ import axios from "axios";
 
 const StateContext = createContext();
 
-export const StateContextProvider = () => {};
+export const StateContextProvider = () => {
+  const [weather, setWeather] = useState({});
+  const [values, setValues] = useState([]);
+  const [place, setPlace] = useState("Paris");
+  const [location, setLocation] = useState("");
+};
