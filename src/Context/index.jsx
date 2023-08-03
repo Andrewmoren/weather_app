f94b2cdd93c82f694bf7bdbdbea2853ee089b558
Andrew <andrewmoren93@gmail.com>
2023-08-03 19:11:24 +0300

    create context

---
+++ src/Context/index.jsx
@@ -0,0 +1,7 @@
+import { useContext, createContext, useState, useEffect } from "react";
+
+import axios from "axios";
+
+const StateContext = createContext();
+
+export const StateContextProvider = () => {};
