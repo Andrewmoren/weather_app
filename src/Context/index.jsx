8fccec68feda20ef079a4fc1289bb1c4eeeb42d9
Andrew <andrewmoren93@gmail.com>
2023-08-03 19:20:38 +0300

    axios

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -9,4 +9,20 @@ export const StateContextProvider = () => {
   const [values, setValues] = useState([]);
   const [place, setPlace] = useState("Paris");
   const [location, setLocation] = useState("");
+
+  //fetch api
+
+  const fetchWeather = async () => {
+    const options = {
+      method: "GET",
+      url: "https://visual-crossing-weather.p.rapidapi.com/forecast",
+      params: {
+        aggregateHours: "24",
+        location: place,
+        ContentType: "json",
+        unitGroup: "metric",
+        shortColumnNames: 0,
+      },
+    };
+  };
 };
