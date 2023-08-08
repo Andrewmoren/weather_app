d05b4afed075da9419fc7846726bc1273067b13a
Andrew <andrewmoren93@gmail.com>
2023-08-08 21:16:11 +0300

    add imports

--- src/Components/BackgroundLayout.jsx
+++ src/Components/BackgroundLayout.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import { useStateContext } from "../Context";
 
+//images
+
+import Clear from "../assets/images/Clear.jpg";
+import Fog from "../assets/images/fog.png";
+import Cloudy from "../assets/images/Cloudy.jpg";
+import Rainy from "../assets/images/Rainy.jpg";
+import Snow from "../assets/images/snow.jpg";
+import Stormy from "../assets/images/Stormy.jpg";
+import Sunny from "../assets/images/Sunny.jpg";
+
 const BackgroundLayout = () => {
   const { weather } = useStateContext();
+  const [image, setImage] = useState(Clear);
 
-  console.log(weather);
   return <div></div>;
 };
 
