c05ba23668266dfa0dd3ff81930525ee10b7ad21
Andrew <andrewmoren93@gmail.com>
2023-08-11 23:48:07 +0300

    create weather care imports

--- src/Components/WeatherCard.jsx
+++ src/Components/WeatherCard.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import { useDate } from "../Utils/useDate";
+
+import sun from "../assets/icons/sun.png";
+import cloud from "../assets/icons/cloud.png";
+import fog from "../assets/icons/fog.png";
+import rain from "../assets/icons/rain.png";
+import snow from "../assets/icons/snow.png";
+import storm from "../assets/icons/storm.png";
+import wind from "../assets/icons/wind.png";
 
 const WeatherCard = (
   temperature,
