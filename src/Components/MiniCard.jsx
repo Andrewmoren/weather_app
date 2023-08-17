f77d1a50eb2f140725af8b8c4180ff828753d730
Andrew <andrewmoren93@gmail.com>
2023-08-17 06:55:18 +0300

    create mini card

--- src/Components/MiniCard.jsx
+++ src/Components/MiniCard.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const MiniCard = () => {
-  return <div></div>;
+import sun from "../assets/icons/sun.png";
+import cloud from "../assets/icons/cloud.png";
+import fog from "../assets/icons/fog.png";
+import rain from "../assets/icons/rain.png";
+import snow from "../assets/icons/snow.png";
+import storm from "../assets/icons/storm.png";
+import wind from "../assets/icons/wind.png";
+
+const MiniCard = ({ time, temp, iconString }) => {
+  const [icon, setIcon] = useState();
+
+  useEffect(() => {
+    if (iconString.toLowerCase().includes("cloud")) {
+      setIcon(cloud);
+    } else if (iconString.toLowerCase().includes("rain")) {
+      setIcon(rain);
+    } else if (iconString.toLowerCase().includes("clear")) {
+      setIcon(sun);
+    } else if (iconString.toLowerCase().includes("thunder")) {
+      setIcon(storm);
+    } else if (iconString.toLowerCase().includes("fog")) {
+      setIcon(fog);
+    } else if (iconString.toLowerCase().includes("snow")) {
+      setIcon(snow);
+    } else if (iconString.toLowerCase().includes("wind")) {
+      setIcon(wind);
+    }
+  }, [iconString]);
+  return (
+    <div className="glassCard w-[10rem] h-[10rem] p-4 flex flex-col">
+      <p className="text-center"></p>
+    </div>
+  );
 };
 
 export default MiniCard;
